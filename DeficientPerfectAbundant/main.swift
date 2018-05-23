1cd86bfaa69e438a32c57ad0f976c25767d0b628
Lee Andrew <alee@lcs-mba3087.lcs.on.ca>
2018-05-22 21:46:32 -0400

    another one that I forgot to commit

--- DeficientPerfectAbundant/main.swift
+++ DeficientPerfectAbundant/main.swift
@@ -5,13 +5,56 @@
 //  Created by Gordon, Russell on 2018-04-06.
 //  Copyright © 2018 Gordon, Russell. All rights reserved.
 //
-
 import Foundation
 
-// Get the user input
-var rawInput = readLine()
+// Prompt the user
+print("Input number is? ")
 
-// Print out the input provided
-print("You said:")
-print(rawInput)
+// Input //
+var numbers = 0
+
+// forever prompt the user
+while 1 == 1 {
+    
+    // Process //
+    // Test #1: Check if nil
+    guard let givenInput = readLine() else {
+        continue
+    }
+    
+    // Test #2: Check if integer
+    guard let intInput = Int(givenInput) else {
+        continue
+    }
+    
+    // Test #3: Check if within range
+    if intInput < 1 || intInput > 32500 {
+        continue
+    }
+    
+    // Once it goes through all the tests, it puts the number into a variable and breaks the process
+    givenInteger = intInput
+    break
+}
+
+// Output
+var Divisors = 0
+
+for i in 2...numbers{
+    
+    if numbers % i == 0 {
+        Divisors += i
+    }
+}
+
+// comparing the total of the divisors to the number to determine what kind of number it is.
+if totalDivisor < numbers {
+    print("\(numbers) is a deficient")
+} else if Divisors > numbers{
+    print("\(numbers) is an abundant")
+} else if Divisors == numbers {
+    print("\(numbers) is a perfect number")
+}
+
+
 
